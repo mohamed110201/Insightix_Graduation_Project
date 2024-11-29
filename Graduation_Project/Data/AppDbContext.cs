8e75d1363fb20038b8b10f44d50dc5798fec1631
MohamedAlkady <mohamedalkady65@gmail.com>
2024-11-29 23:38:37 +0200

    prepare DbContext and ConnectionString

--- Graduation_Project/Data/AppDbContext.cs
+++ Graduation_Project/Data/AppDbContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Graduation_Project.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Graduation_Project.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
