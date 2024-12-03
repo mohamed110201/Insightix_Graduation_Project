318ed64d9ed2bd3c0be52deed4e2f9c2f61bc797
MohamedAlkady <mohamedalkady65@gmail.com>
2024-12-04 01:30:47 +0200

    fix error in SeedDataLoader

--- Graduation_Project/Data/Seed/SeedDataLoader.cs
+++ Graduation_Project/Data/Seed/SeedDataLoader.cs
@@ -11,11 +11,17 @@ public class SeedDataLoader(AppDbContext db)
     private void SaveToDb(SeedDataContainer data)
     {
         db.Systems.AddRange(data.Systems);
+        db.SaveChanges();
         db.MachineTypes.AddRange(data.MachineTypes);
+        db.SaveChanges();
         db.MonitoringAttributes.AddRange(data.MonitoringAttributes);
+        db.SaveChanges();
         db.ResourceConsumptionAttributes.AddRange(data.ResourceConsumptionAttributes);
+        db.SaveChanges();
         db.MachineTypeMonitoringAttributes.AddRange(data.MachineTypeMonitoringAttributes);
+        db.SaveChanges();
         db.MachineTypeResourceConsumptionAttributes.AddRange(data.MachineTypeResourceConsumptionAttributes);
+        db.SaveChanges();
         db.Machines.AddRange(data.Machines);
         db.SaveChanges();
     }
