426ae445fe4d8ae0062a1f2ac9593a63c8a7b656
MohamedAlkady <mohamedalkady65@gmail.com>
2024-12-04 01:28:07 +0200

    add MachineTypeResourceConsumptionAttributeConfig

---
+++ Graduation_Project/Data/Config/MachineTypeResourceConsumptionAttributeConfig.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Graduation_Project.Data.Config
+{
+    public class MachineTypeResourceConsumptionAttributeConfig : IEntityTypeConfiguration<MachineTypeResourceConsumptionAttribute>
+    {
+        public void Configure(EntityTypeBuilder<MachineTypeResourceConsumptionAttribute> builder)
+        {
+            builder.ToTable("MachineTypeResourceConsumptionAttributes");
+        }
+    }
+}
