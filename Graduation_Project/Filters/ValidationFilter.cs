29e99bb7d32d0c0c4574a19f8cef8e6baa37e7a3
MohamedAlkady <mohamedalkady65@gmail.com>
2024-12-07 20:56:06 +0200

    create FluentValidation Filter

---
+++ Graduation_Project/Filters/ValidationFilter.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Graduation_Project.Filters;
+
+public class ValidationFilter : IActionFilter
+{
+    public void OnActionExecuted(ActionExecutedContext context) { }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (context.ModelState.IsValid) return;
+        
+        var messages = context.ModelState
+            .SelectMany(message => message.Value!.Errors)
+            .Select(error => error.ErrorMessage)
+            .ToList();
+           
+        context.Result = new BadRequestObjectResult(messages);
+    }
+}
