1a6b8b6cd241b28973a539f5d23c127bccadb67f
MaryemMagdy <mariammagdyhasehim@gmail.com>
2025-04-29 01:49:05 +0300

    JSendCommits & Reducing ExpiryDate

--- Graduation_Project/Modules/Authentication/DTOs/LoginUserDto.cs
+++ Graduation_Project/Modules/Authentication/DTOs/LoginUserDto.cs
@@ -5,7 +5,7 @@ namespace Graduation_Project.Modules.Authentication.DTOs
     public class LoginUserDto
     {
        
-        public string Username { get; set; }
+        public string Email { get; set; }
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
