5b26d216348c796526c4937bd205b077168d8b29
MaryemMagdy <mariammagdyhasehim@gmail.com>
2025-04-28 16:32:52 +0300

    LogOut_Endpoint

--- Graduation_Project/Modules/Authentication/AuthenticationController.cs
+++ Graduation_Project/Modules/Authentication/AuthenticationController.cs
@@ -1,5 +1,7 @@
 ﻿using Graduation_Project.Modules.Authentication.DTOs;
 using Graduation_Project.Modules.Authentication.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +24,17 @@ namespace Graduation_Project.Modules.Authentication
 
             var token = authService.GenerateJwtToken(user.UserName);
             return Ok(new { token });
+        }
+        [HttpPost("logout")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+
+        public async Task<IActionResult> Logout()
+        {
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+            await authService.BlacklistTokenAsync(token); 
+
+            return Ok(new { message = "Logged out successfully." });
         }
     }
 }
